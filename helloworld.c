e13f512bcad283f4b3921e6b7ee7ef4c3eb67d1a
Pooja Goyal <2020csb1108@iitrpr.ac.in>
2024-01-13 00:41:26 +0530

    WASM - Helloworld and compiling a library

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <emscripten.h>
+
+EMSCRIPTEN_KEEPALIVE
+int addNums(int a, int b){
+    return a+b;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int main(){
+
+    printf("Hello World, %d\n", addNums(3, 5));
+
+    return 0;
+}
